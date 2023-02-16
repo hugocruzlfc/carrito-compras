2b7da2e026817bd3be90b3795bcf3c66fab508f8
hde_la_torre <hugo.de_la_torre@sita.aero>
2023-02-16 14:35:05 +0100

    Upload all project

---
+++ src/components/Filters.jsx
@@ -0,0 +1,59 @@
+import { useId } from "react";
+import { useFilters } from "../hooks/useFilters.js";
+import "./Filters.css";
+
+export function Filters() {
+  const { filters, setFilters } = useFilters();
+
+  //const [minPrice, setMinPrice] = useState(0); // estado local
+  const minPriceFilterId = useId();
+  const categoryFilterId = useId();
+
+  const handleChangeMinPrice = (event) => {
+    //setMinPrice(event.target.value);  // mala practica, tenemos dos estados
+
+    setFilters((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
+
+  const handleChangeCategory = (event) => {
+    // ⬇️ ESTO HUELE MAL
+    // estamos pasando la función de actualizar estado
+    // nativa de React a un componente hijo
+    setFilters((prevState) => ({
+      ...prevState,
+      category: event.target.value,
+    }));
+  };
+
+  return (
+    <section className="filters">
+      <div>
+        <label htmlFor={minPriceFilterId}>Precio a partir de:</label>
+        <input
+          type="range"
+          id={minPriceFilterId}
+          min="0"
+          max="1000"
+          onChange={handleChangeMinPrice}
+          value={filters.minPrice}
+        />
+        <span>${filters.minPrice}</span>
+      </div>
+
+      <div>
+        <label htmlFor={categoryFilterId}>Categoría</label>
+        <select
+          id={categoryFilterId}
+          onChange={handleChangeCategory}
+        >
+          <option value="all">Todas</option>
+          <option value="laptops">Portátiles</option>
+          <option value="smartphones">Celulares</option>
+        </select>
+      </div>
+    </section>
+  );
+}
